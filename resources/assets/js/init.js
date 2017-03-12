3016fe77ac9a13291c467b0ea5740262e53ad720
muffeen <jeff_magboo19@yahoo.com>
2017-03-13 00:46:16 +0800

    Fix url hash bug

--- resources/assets/js/init.js
+++ resources/assets/js/init.js
@@ -1,10 +1,15 @@
 
 $(function() {
 
-    history.pushState("", document.title, window.location.pathname
-        + window.location.search);
-
     var url = window.location;
+
+    console.log(url.href);
+
+    if (url.href.endsWith("/#")) {
+        history.pushState("", document.title, window.location.pathname
+            + window.location.search);
+    }
+
     var element = $('ul.nav a').filter(function() {
         return this.href == url;
     }).parent().addClass('active');
