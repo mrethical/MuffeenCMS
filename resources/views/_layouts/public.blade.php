fa101ded8556050f8cfd41bf02c974097aa2dda3
muffeen <jeff_magboo19@yahoo.com>
2017-04-09 23:26:29 +0800

    Add navbar brand link to home

--- resources/views/_layouts/public.blade.php
+++ resources/views/_layouts/public.blade.php
@@ -30,7 +30,7 @@
                     <span class="icon-bar"></span>
                     <span class="icon-bar"></span>
                 </button>
-                <a class="navbar-brand" href="#">{{ config('app.name') }}</a>
+                <a class="navbar-brand" href="{{ url('/') }}">{{ config('app.name') }}</a>
             </div>
             <div id="navbar" class="navbar-collapse collapse">
                 <ul class="nav navbar-nav navbar-right">
