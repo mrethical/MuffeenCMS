2bf7a9f6a4ab8a36e0d9a5e0d00fb51b1b1d41e0
muffeen <jeff_magboo19@yahoo.com>
2017-04-10 10:29:03 +0800

    Fix document outline

--- resources/views/pages/index.blade.php
+++ resources/views/pages/index.blade.php
@@ -4,7 +4,6 @@
 
     <section class="row">
         <div class="col-md-12">
-            <h1 class="hidden">{{ $page->title }}</h1>
             <?= $page->content ?>
         </div>
     </section>
